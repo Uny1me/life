85636bc964fb70023d79793a4f0732cc85e76b39
Unyime Emmanuel <unyimeemmanuel012@gmail.com>
2025-05-01 20:16:54 +0100

    updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d1125
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1389012563"
+    serviceWorkerVersion: "2895216343"
   }
 });
